d99cc280b3cd495f06e604385ae7b1edec1cc6e7
Thobani Mabaso <zxinwa023@student.wethinkcode.co.za>
2025-01-20 13:15:59 +0200

    added an interface to interact with the database

--- src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/repository/WasteCategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/repository/WasteCategoryRepository.java
@@ -1,4 +1,7 @@
 package com.enviro.assessment.grad001.ThobaniMabaso.repository;
 
-public class WasteCategoryRepository {
+import com.enviro.assessment.grad001.ThobaniMabaso.model.WasteCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WasteCategoryRepository extends JpaRepository<WasteCategory, Long> {
 }
