dc3394dddf1df30b525a135d0368a7a33f84093b
Thobani Mabaso <zxinwa023@student.wethinkcode.co.za>
2025-01-21 15:39:10 +0200

    This is the main(running) application

--- src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/App.java
+++ src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/App.java
@@ -1,6 +1,12 @@
 package com.enviro.assessment.grad001.ThobaniMabaso;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
