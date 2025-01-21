2d681ba318a0869d984711d15df0207df06d25b4
Thobani Mabaso <zxinwa023@student.wethinkcode.co.za>
2025-01-21 15:37:21 +0200

    added a class to handle exceptions

--- src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.enviro.assessment.grad001.ThobaniMabaso.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
