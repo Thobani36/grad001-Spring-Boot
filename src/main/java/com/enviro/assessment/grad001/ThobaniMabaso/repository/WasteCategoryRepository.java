abd78aa3a80d3921737b84a2978b18a3d042e071
Thobani Mabaso <zxinwa023@student.wethinkcode.co.za>
2025-01-23 20:29:50 +0200

    made a few changes on the interface

--- src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/repository/WasteCategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/ThobaniMabaso/repository/WasteCategoryRepository.java
@@ -2,6 +2,11 @@ package com.enviro.assessment.grad001.ThobaniMabaso.repository;
 
 import com.enviro.assessment.grad001.ThobaniMabaso.model.WasteCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WasteCategoryRepository extends JpaRepository<WasteCategory, Long> {
+
+
+
 }
